e57e92e8b240bc0b78666c9ad4ffcfe6b53b9639
miyuko <miyuko@calayucu.com>
2025-05-11 21:00:33 +0100

    apply go fmt

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 )
 
 func main() {
-    if len(os.Args) != 3 {
+	if len(os.Args) != 3 {
 		println("Usage: borkmp4 INPUT.mp4 OUTPUT.mp4")
 		return
 	}
 
-    inputPath := os.Args[1]
+	inputPath := os.Args[1]
 	outputPath := os.Args[2]
 
 	err := editFile(inputPath, outputPath)
@@ -62,40 +62,40 @@ func editFile(inputPath, outputPath string) error {
 
 		// edit ESDS field
 		switch h.BoxInfo.Type {
-    		case mp4.BoxTypeEsds():
-    			esds := box.(*mp4.Esds)
-    			for idx, descr := range esds.Descriptors {
-        			if descr.Tag == mp4.DecSpecificInfoTag {
-            			var objType = descr.Data[0] >> 3
-            			if objType != 2 {
-                			println("esds audio object type was not AAC LC, skipping")
-                			continue
-            			}
-            			var freqIndex = ((descr.Data[0] & 0x7) << 1) + (descr.Data[1] >> 7)
-            			if freqIndex > 0xC {
-                			println("custom frequency indices are not supported, skipping")
-                			continue
-            			}
-            			var channelConfig = (descr.Data[1] & 0x78) >> 3
-            			if channelConfig == 0 {
-                			println("AOT specific config not supported, skipping")
-                			continue
-            			}
+		case mp4.BoxTypeEsds():
+			esds := box.(*mp4.Esds)
+			for idx, descr := range esds.Descriptors {
+				if descr.Tag == mp4.DecSpecificInfoTag {
+					var objType = descr.Data[0] >> 3
+					if objType != 2 {
+						println("esds audio object type was not AAC LC, skipping")
+						continue
+					}
+					var freqIndex = ((descr.Data[0] & 0x7) << 1) + (descr.Data[1] >> 7)
+					if freqIndex > 0xC {
+						println("custom frequency indices are not supported, skipping")
+						continue
+					}
+					var channelConfig = (descr.Data[1] & 0x78) >> 3
+					if channelConfig == 0 {
+						println("AOT specific config not supported, skipping")
+						continue
+					}
 
-            			var audConf [4]byte
-            			audConf[0] = 5 << 3;  // AAC SBR object type (5 bit uint)
-            			audConf[0] += freqIndex >> 1  // freqIndex (4 bit uint)
-            			audConf[1] = (freqIndex & 0x1) << 7
-            			audConf[1] += channelConfig << 3  // channelConfig (4 bit uint)
-            			audConf[1] += freqIndex >> 1  // extFreqIndex
-            			audConf[2] = (freqIndex & 0x1) << 7
-            			audConf[2] += 2 << 2  //  AAC LC object type (extension)
+					var audConf [4]byte
+					audConf[0] = 5 << 3          // AAC SBR object type (5 bit uint)
+					audConf[0] += freqIndex >> 1 // freqIndex (4 bit uint)
+					audConf[1] = (freqIndex & 0x1) << 7
+					audConf[1] += channelConfig << 3 // channelConfig (4 bit uint)
+					audConf[1] += freqIndex >> 1     // extFreqIndex
+					audConf[2] = (freqIndex & 0x1) << 7
+					audConf[2] += 2 << 2 //  AAC LC object type (extension)
 
-            			esds.Descriptors[idx].Data = audConf[:]
-            			esds.Descriptors[idx].Size = 4
-            			println("Breaking your esds atom for you, you're welcome!")
-        			}
-    			}
+					esds.Descriptors[idx].Data = audConf[:]
+					esds.Descriptors[idx].Size = 4
+					println("Breaking your esds atom for you, you're welcome!")
+				}
+			}
 		}
 
 		// write payload
@@ -115,4 +115,3 @@ func editFile(inputPath, outputPath string) error {
 	})
 	return err
 }
-
